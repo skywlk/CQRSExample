860e879c459f8d021b44d891c5c9a9d8e45086a7
vlashkhia <vlashkhia@VLASHKHIA>
2020-07-14 02:23:47 +0400

    Initial commit

---
+++ Command/UsersCommands/CreateUserCommandHandler.cs
@@ -0,0 +1,32 @@
+using System.Threading;
+using System.Threading.Tasks;
+using CQRSMediatR.Domain.Models;
+using CQRSMediatR.Domain.Repositories;
+using MediatR;
+
+namespace CQRSMediatR.Domain.Commands.Users
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, User>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User
+            {
+                Name = request.Name,
+                Age = request.Age,
+            };
+
+            await _userRepository.AddAsync(user);
+
+            return user;
+        }
+    }
+}
